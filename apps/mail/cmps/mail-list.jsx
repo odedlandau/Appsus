3075751a972d3e7142d2e1aaeb6f2d5fda065703
Oded Landau <odedlan8@gmail.com>
2022-08-24 23:59:02 +0300

    last commit for today

--- apps/mail/cmps/mail-list.jsx
+++ apps/mail/cmps/mail-list.jsx
@@ -2,20 +2,12 @@ import { MailPreview } from "./mail-preview.jsx";
 
 
 export function MailList({ emails }) {
-
+    console.log('from email-list', emails);
     return <table className="mail-list">
-        
-            {
-            emails.map(email => {
 
-                
-                <MailPreview key={email.id} email={email} />
-                
-            } 
-                
-                
-                
-                )}
-        
+        {emails.map(email => <MailPreview key={email.id} email={email} />)}
+
     </table>
+
+   
 }
--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,11 +1,12 @@
 
 
 export function MailPreview({ email }) {
-
+    
     return <tr className="mail-preview">
-        <button>X</button>
+        <td><button>X</button></td>
         <td>{email.from.userName}</td>
         <td>{email.subject}</td>
         <td>{email.sentAt}</td>
-    </tr>
+    </tr> 
+     
 }
